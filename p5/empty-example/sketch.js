bc5acedb02754a2a527dbf35933b5c24343bc670
Andy Dayton <amdayton@gmail.com>
2015-10-30 07:10:55 -0400

    Update p5 to latest

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,7 +1,9 @@
 function setup() {
-  // put setup code here
+  // uncomment this line to make the canvas the full size of the window
+  // createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  // put drawing code here
+  // draw stuff here
+  // ellipse(width/2, height/2, 50, 50);
 }
