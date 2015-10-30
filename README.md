5a1db18e47601eccf73ba2ca121e12d358994b55
Andy Dayton <amdayton@gmail.com>
2015-10-30 07:08:02 -0400

    Update readme and example

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# clock
-Third assignment.
+# Timepiece
+
+## Assignment
+
+Create a time-based composition for the browser. It should _respond_ to time, but doesn't need to be a _clock_.
+
+ * [Download this project here](https://github.com/parsons-cc/timepiece/archive/gh-pages.zip) to get started.
+ * [View the sample project in your browser](http://cc.andydayton.com/timepiece/).
+
+### Resources
+
+You can use the date/time functions provided by p5.js:
+
+ * [`day()`](http://p5js.org/reference/#/p5/day)
+ * [`hour()`](http://p5js.org/reference/#/p5/hour)
+ * [`minute()`](http://p5js.org/reference/#/p5/minute)
+ * [`millis()`](http://p5js.org/reference/#/p5/millis)
+ * [`month()`](http://p5js.org/reference/#/p5/month)
+ * [`second()`](http://p5js.org/reference/#/p5/second)
+ * [`year()`](http://p5js.org/reference/#/p5/year)
+
+### Grading
+
+Your finished project must satisfy the following criteria:
+
+ 1. It should load in the browser (it should work)
+ 2. It should change over time
+ 3. It should be uploaded to a publicly available server (your class site via GitHub)
+
+You will also be graded on overall execution.
+
+## Project Setup
+
+For basic functionality you can just open index.html in your browser! If you want to do things like load images, video, or sound then you might want to use the local server instructions below.
+
+### Local server
+
+If you want to use more of p5js's functionality, you may need to run a local server for development. [You can find instructions here.](https://gist.github.com/daytona1/3a687b46a43236055819)
--- script.js
+++ script.js
@@ -1,78 +1,45 @@
-/**
- * Here's where our JavaScript goes
- */
-
-// We still define our variables here
-var lastSecond;
-var c;
-
-/**
- * We're using the p5 "instance mode" for this new structure
- * http://p5js.org/learn/examples/Instance_Mode_Instantiation.php
- * So that we can use the function attribute `sketch` to access p5 functions
- */
-var mySketch = new p5(function(sketch) {
 
 
-	function getRandomColor() {
-		var r = sketch.random(0,255);
-		var g = sketch.random(0,255);
-		var b = sketch.random(0,255);
-
-		return sketch.color(
-			sketch.floor(r),
-			sketch.floor(g),
-			sketch.floor(b)
-		);
-	};
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
 
 
-	function mousePressed() {
-		var isFullscreen = sketch.fullscreen();
-		sketch.fullscreen(!isFullscreen);
+function draw() {
+	background(0);
+	noStroke();
+
+	fill(255, 0, 0);
+	ellipse(width/4, height/2, second()*2, second()*2);
+	fill(0, 255, 0);
+	ellipse(2*width/4, height/2, minute()*2, minute()*2);
+	fill(0, 0, 255);
+	ellipse(3*width/4, height/2, hour()*10, hour()*10);
+
+
+	fill(255);
+	textSize(50);
+	textAlign(CENTER);
+
+	var hourText = hour();
+	var minuteText = setDigits(minute(), 2);
+	var secondText = setDigits(second(), 2);
+
+	text(hourText+':'+minuteText+':'+secondText, width/2, height/3);
+
+}
+
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+
+function setDigits(number, digits) {
+	var str = number+'';
+
+	while(str.length < digits) {
+		str = '0'+str;
 	}
 
-
-	function windowResized() {
-		sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight);
-	}
-
-	/**
-   * Same as `function setup() {}` before, but written a different way
-   * using our namespace.
-   */
-	sketch.setup = function() {
-		sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
-		lastSecond = sketch.second();
-		c = getRandomColor();
-	}
-
-	/**
-   * Same as `function setup() {}` before, but written a different way
-   * using our namespace.
-   */
-	sketch.draw = function() {
-		sketch.background(0,0,0);
-
-		if(sketch.lastSecond !== sketch.second()) {
-			sketch.lastSecond = sketch.second();
-			c = getRandomColor();
-		}
-
-		sketch.fill(c);
-		// random rect
-		sketch.rect(
-			sketch.minute(),
-			sketch.second(),
-			sketch.day()+sketch.hour(),
-			sketch.month()+sketch.hour()
-		);
-
-		sketch.textSize(20);
-		sketch.fill(255,255,255);
-		sketch.noStroke();
-		sketch.text(sketch.hour()+":"+sketch.minute()+":"+sketch.second(), 10, sketch.windowHeight-10);
-	}
-
-
-});
+	return str
+}
